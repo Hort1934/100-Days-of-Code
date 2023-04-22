67393964810b6d2de9014549f8a260d4a6f1b8e5
Ihor Marchenko <igormarchenko1934@gmail.com>
2023-04-22 22:27:34 +0300

    added new study files Day_35

--- Day_35/task_1.py
+++ Day_35/task_1.py
@@ -1,13 +1,37 @@
 import requests
+from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-api_key = "618705b92bc1e04bb5267cab96095339"
+api_key = "69f04e4613056b159c2761a9d9e664d2"
+account_sid = "AC899afe95efb241928f6c35a4d6bb9557"
+auth_token = "95d613570ae53eb2d90e225800c52d7e"
 
 parameters_of_weather = {
-    "lat": 50.447731,
-    "lon": 30.542721,
-    "appid": api_key
+    "lat": 51.507351,
+    "lon": -0.127758,
+    "appid": api_key,
+    "exlude": "current,minutely,daily"
 }
 
 response = requests.get(OWM_Endpoint, params=parameters_of_weather)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["hourly"][0]["weather"][0]["id"])
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It`s going to rain today. Remember to bring an umbrella",
+        from_='+16813396341',
+        to='+380 63 410 9546'
+    )
+    print(message.status)
