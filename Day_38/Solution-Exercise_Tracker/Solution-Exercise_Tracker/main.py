5a398337ceea658a2d03b3fe55b1c2c6609c5d11
Ihor Marchenko <igormarchenko1934@gmail.com>
2023-04-26 22:41:26 +0300

    added new study files Day_38

---
+++ Day_38/Solution-Exercise_Tracker/Solution-Exercise_Tracker/main.py
@@ -0,0 +1,85 @@
+import requests
+from datetime import datetime
+import os
+
+# Your personal data. Used by Nutritionix to calculate calories.
+GENDER = "male"
+WEIGHT_KG = 84
+HEIGHT_CM = 180
+AGE = 32
+
+# Nutritionix APP ID and API Key. Actual values are stored as environment variables.
+APP_ID = os.environ["e53c2cc1"]
+API_KEY = os.environ["367fac05ded98b3acb54d018205cc773"]
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+
+exercise_text = input("Tell me which exercises you did: ")
+
+# Nutritionix API Call
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(f"Nutritionix API call: \n {result} \n")
+
+# Adding date and time
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+# Sheety Project API. Check your Google sheet name and Sheety endpoint
+GOOGLE_SHEET_NAME = "workout"
+sheet_endpoint = os.environ[
+    "https://api.sheety.co/95d949299830cb0ec6024f9a48eab1b1/myWorkouts/workout"]
+
+# Sheety API Call & Authentication
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        GOOGLE_SHEET_NAME: {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    # Sheety Authentication Option 1: No Auth
+    """
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+    """
+
+    # Sheety Authentication Option 2: Basic Auth
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        auth=(
+            os.environ["AppBreweryInfo"],
+            os.environ["MySecretPassword!Mwahahah"],
+        )
+    )
+
+    # Sheety Authentication Option 3: Bearer Token
+    """
+    bearer_headers = {
+        "Authorization": f"Bearer {os.environ['aWhvcm06U0VoMjZmWTl0Nio=']}"
+    }
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        headers=bearer_headers
+    )    
+    """
+    print(f"Sheety Response: \n {sheet_response.text}")
