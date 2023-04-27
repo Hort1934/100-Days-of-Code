03ea30605959c322c26a792100cfd9bb19de7cf0
Ihor Marchenko <igormarchenko1934@gmail.com>
2023-04-27 20:08:14 +0300

    added new study files Day_39

---
+++ Day_39/flight-deals-end/notification_manager.py
@@ -0,0 +1,21 @@
+from twilio.rest import Client
+
+TWILIO_SID = "AC899afe95efb241928f6c35a4d6bb9557"
+TWILIO_AUTH_TOKEN = "d14592fbd47ffe42e3fb4988b1ac0516"
+TWILIO_VIRTUAL_NUMBER = "+16813396341"
+TWILIO_VERIFIED_NUMBER = "+380 63 410 9546"
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
