f53065b247c36393919bd551f1eceb8db8891d56
Ihor Marchenko <igormarchenko1934@gmail.com>
2023-05-18 23:31:34 +0300

    inside changes

--- Day_59/Solution+&+Completed+Code+day-59-end/main.py
+++ Day_59/Solution+&+Completed+Code+day-59-end/main.py
@@ -3,8 +3,8 @@ import smtplib
 import requests
 
 posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
-OWN_EMAIL = YOUR OWN EMAIL ADDRESS
-OWN_PASSWORD = YOUR EMAIL ADDRESS PASSWORD
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
 app = Flask(__name__)
 
 @app.route('/')
